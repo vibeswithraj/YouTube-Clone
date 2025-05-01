2764fdb045d3f9592fe6de9c816471f819e78520
vibeswithraj <rajbhai82581@gmail.com>
2025-05-01 15:52:52 +0530

    added database url3

--- Backend/Routes/Videos.js
+++ Backend/Routes/Videos.js
@@ -1,20 +1,29 @@
-import express, { urlencoded } from "express";
+import express, { urlencoded } from 'express';
 export const router = express.Router();
-import cors from "cors";
-import { allData } from "../data.js";
-import dotenv from "dotenv";
-import { connectDatabase } from "../Utilities/Database.js";
-import { profile, signup, sendVideo, subscribe, subscriptions } from "../Controllers/videos.js";
-import cookieParser from "cookie-parser";
+import cors from 'cors';
+import { allData } from '../data.js';
+import dotenv from 'dotenv';
+import { connectDatabase } from '../Utilities/Database.js';
+import {
+  profile,
+  signup,
+  sendVideo,
+  subscribe,
+  subscriptions,
+} from '../Controllers/videos.js';
+import cookieParser from 'cookie-parser';
 // import bodyParser from "body-parser";
 dotenv.config({
-  path: "./config.env",
+  path: './config.env',
 });
 
 router.use(
   cors({
-    methods: ["GET", "POST"],
-    origin: true,
+    origin: [
+      'http://localhost:3000',
+      'https://you-tube-clone-six-eta.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
 );
@@ -24,12 +33,12 @@ router.use(urlencoded({ extended: true }));
 
 connectDatabase();
 
-router.get("/data", (req, res) => {
+router.get('/data', (req, res) => {
   res.json(allData);
 });
 
-router.post("/signup", signup);
-router.get("/me", profile);
-router.get("/video/:id", sendVideo);
-router.get("/subscribe/:id", subscribe);
-router.get("/subscriptions", subscriptions);
+router.post('/signup', signup);
+router.get('/me', profile);
+router.get('/video/:id', sendVideo);
+router.get('/subscribe/:id', subscribe);
+router.get('/subscriptions', subscriptions);
