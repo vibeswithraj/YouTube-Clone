2e53da1078abff60f35b91067a2ea4f59c67fde5
vibeswithraj <rajbhai82581@gmail.com>
2025-05-01 15:07:01 +0530

    added database url

--- Backend/Utilities/Database.js
+++ Backend/Utilities/Database.js
@@ -1,12 +1,11 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config({ path: "./config.env" });
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config({ path: './config.env' });
 export const connectDatabase = () => {
   mongoose
     .connect(process.env.MONGODB_URL, {
-      dbName: "Youtube",
+      dbName: 'Youtube',
     })
-    .then(() => console.log("Database is connected..."))
+    .then(() => console.log('Database is connected...'))
     .catch((error) => console.log(error));
 };
